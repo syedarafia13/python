104e30a9ab9053124b291fa48c706d3b0beb92c7
Syeda Rafia Fizza Naveed <81455748+syedarafia13@users.noreply.github.com>
2021-09-10 20:48:04 +0500

    Add files via upload

---
+++ 01_variable.srfn.py
@@ -0,0 +1,24 @@
+# Author: SYEDA RAFIA FIZZA NAVEED
+# Program of Variables and Data types in python.
+
+'''  ********************* VARIABLES *************************  '''
+a_13 = 'Rafia Fizza'
+# a = 'Rafia Fizza'
+# a = "Rafia Fizza"
+# a = '''Rafia Fizza'''
+b = 53
+c = 1309.2000
+d = True
+# d = None
+
+'''  ***************** PRINTING THE VARIABLES ****************  '''
+print(a_13)
+print(b)
+print(c)
+print(d)
+
+'''  ************ PRINTING THE TYPES OF VARIABLES *************  '''
+print(type(a_13))
+print(type(b))
+print(type(c))
+print(type(d))
