af03f5c7dcce841cae30f8e3e564da9f85eca346
Syeda Rafia Fizza Naveed <81455748+syedarafia13@users.noreply.github.com>
2021-09-10 20:46:41 +0500

    Add files via upload

---
+++ 01_hello_srfn.py
@@ -0,0 +1,10 @@
+# Author: SYEDA RAFIA FIZZA NAVEED
+# Program to print Hello World.
+'''
+*************************************************
+'''
+import os            #Importing the os module
+print("Hello world")
+'''
+**************************************************
+'''
